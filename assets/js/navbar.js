c84e7a1b2a026f6bb1c38fa8db72dd469176864c
Raheeemsala7 <rahemsalah534@gmail.com>
2024-11-15 18:26:18 +0200

    review for ui/ux

--- assets/js/navbar.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('toggle-button').addEventListener('click', function () {
-    const navCollapse = document.getElementById('navbarSupportedContent');
-    const isExpanded = this.getAttribute('aria-expanded') === 'true';
-
-    navCollapse.classList.toggle('show');
-    this.setAttribute('aria-expanded', !isExpanded);
-    document.getElementById('toggler-icon').innerHTML = isExpanded ? '&#9776;' : '&#10006;';
-});
